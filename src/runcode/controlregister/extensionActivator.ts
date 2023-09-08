abf9f44006a395c6c11399edcd0c1856a0ff9acb
muchaja3 <muchaja3@fel.cvut.cz>
2023-09-09 00:18:01 +0200

    implement Control Register

---
+++ src/runcode/controlregister/extensionActivator.ts
@@ -0,0 +1,17 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { registerCommand as registerCommand } from './commandRegister';
+import { correct } from '../spacescorrector/correctionController';
+
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	console.log('Congratulations, your extension "helloworld" is now active!');
+
+	// The command has been defined in the package.json file. The commandId parameter must match.
+	const commandId = 'nbspcorrector.helloWorld';
+	const command = correct;
+	registerCommand(commandId, command, context);
+}
+
+export function deactivate() {}
